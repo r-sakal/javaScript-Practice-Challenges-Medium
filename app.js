84c9ba1927b230c8c1eda8a2fd91c72b5446e5a1
Rob <rob.sakal23@gmail.com>
2025-06-25 21:04:47 -0400

    medium challange q1

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 //Q1 Falsy or Truthy?
 //  Given two values, return the first one if it is falsy, otherwise return the second one.
+function filterOutFalsy(val1, val2) {
+    return !val1 ? val1 : val2
+}
+
+console.log(filterOutFalsy(0, 500))
