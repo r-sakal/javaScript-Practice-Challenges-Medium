35b8ea745f3493cd37e15ffdbabfce2096858549
Rob <rob.sakal23@gmail.com>
2025-06-25 21:15:41 -0400

    medium challenge q3

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ function filterOutFalsy(val1, val2) {
 }
 
 console.log(filterOutFalsy(0, 500))
+
+//Q2 Return the length of any given array
+//  given an array, return its length
+function arrLength(arr){
+return arr.length
+}
+
+console.log(arrLength([1,2,3,4]))
+
+//Q3 Get the last element in an array
+//  Given an array, return the last element.
