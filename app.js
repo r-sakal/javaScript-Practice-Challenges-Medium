0bd66411934619017e6737af9ee8c94e73d6952f
Rob <rob.sakal23@gmail.com>
2025-06-27 22:56:44 -0400

    Q8 Reverse a string

--- app.js
+++ app.js
@@ -65,3 +65,24 @@ function calcTime(seconds) {
 }
 
 console.log(calcTime(50))
+
+//Q7 Find the largest number
+//  Given an array of numbers, return the largest number of that array.
+function getMax(bigNum) {
+    for (let i = 0; i < bigNum.length; ++i)
+    //  this is what I came up with after googling how to sort an array!
+        bigNum.sort((a, b) => a-b);
+        return bigNum[bigNum.length - 1];
+
+}
+
+// the method showed in the video
+// let max = arr[0]
+// for (let i = 1; i< arr.length; ++i) {
+//      if (arr[i] > max) {
+//      max = arr[i];
+//     }
+//     return max;
+// }
+
+console.log(getMax([500, 100, 60]))
